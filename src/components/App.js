6c5d18589abf6e23bb4ef942d05297d73b695399
Grish <grish.ayvazyan@gmail.com>
2019-05-01 22:37:43 +0400

    Add functional , styles and more

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,46 @@
-import React , {Component} from 'react';
+import React, {Component} from 'react';
 import Cards from '../components/Cards';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
- render() {
+    state = {
+        countArray: []
+    };
+    handleClick = () => {
+        // console.log(this.state.countArray);
+        this.setState(oldState => ({
+            countArray: [...oldState.countArray, +this.state.countArray.length],
+        }));
+    };
 
-   return (
-       <div className="container-fluid">
-           <div className="container">
-                <div className="row">
-                    <button onClick={this.addCard} className="btn btn-outline-info d-block mx-auto mt-5">Add Cards</button>
-                    <div id="Cards" className="col-xl-12">
-                            <Cards/>
+    render() {
+        // console.log(this.state.countArray.length);
+        return (
+            <div className="container-fluid">
+                <div className="container">
+                    <div className="col-xl-12 my-5">
+                        <button onClick={this.handleClick} className="btn btn-outline-info d-block mx-auto">Add
+                            Cards {this.state.countArray.length <= 0 ? 0 : +this.state.countArray.length }
+                        </button>
+                    </div>
+                    <div className="row">
+                        {this.state.countArray.map((key) => {
+                            return (
+                                <Cards key={key}/>
+                            )
+                            })
+                        }
                     </div>
                 </div>
-           </div>
-       </div>
-   )
- }
- addCard = () => {
-     // document.getElementById("Cards").
- }
+            </div>
+        )
+    }
+
+    // addCard = () => {
+    //     this.setState({
+    //         count: this.state.count + 1
+    //     })
+    // }
 }
+
 export default App;
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,16 +8,17 @@ class Cards extends Component {
 
     render() {
         return (
-            <div className="card w-25 text-center">
-                <div className="card-title mt-3">
-                    <h5 style={this.state}> <b>Hello World!</b></h5>
+            <div className="col-xl-3 col-lg-4 col-md-6">
+                <div className="card text-center mb-3" style={{borderColor:this.state.color}}>
+                    <div className="card-title mt-3">
+                        <h5 style={this.state}> <b>Hello World!</b></h5>
+                    </div>
+                    <div className="card-body">
+                        <button className="btn btn-outline-danger" onClick={this.handleClick}>
+                            Change color
+                        </button>
+                    </div>
                 </div>
-                <div className="card-body">
-                    <button className="btn btn-outline-danger" onClick={this.handleClick}>
-                        Change color
-                    </button>
-                </div>
-
             </div>
         )
     }
